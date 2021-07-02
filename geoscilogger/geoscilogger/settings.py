9e11e18c42bd91641b96c36c4ae3d5a87855d058
Bearzilasaur <mitchell.baum@gmail.com>
2021-07-02 11:37:14 +1000

    added/activated djanog-taggit

--- geoscilogger/geoscilogger/settings.py
+++ geoscilogger/geoscilogger/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'taggit' # blog post tags from django-taggit
 ]
 
 MIDDLEWARE = [
