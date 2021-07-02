f527967a6facc0539844346b8b12493f52fbe1b1
Bearzilasaur <mitchell.baum@gmail.com>
2021-07-02 11:38:21 +1000

    working on building blog/log model

--- geoscilogger/samplelogger/models.py
+++ geoscilogger/samplelogger/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.db.models.fields import DateField, DateTimeField
+from taggit.manager import TaggableManager
 
 # Create your models here.
+class log(models.Model):
+    title
+    date
+    content
+    datetime = DateTimeField()
+    tags = TaggableManager()
+
